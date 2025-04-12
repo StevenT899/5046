613698e20ffce70e1b886ce0231a402d1d277bf6
yudeshui <dyuu0013@student.monash.edu>
2025-04-12 17:23:40 +1000

    the login and register

--- app/src/main/java/com/example/a5046/MainActivity.kt
+++ app/src/main/java/com/example/a5046/MainActivity.kt
@@ -4,14 +4,41 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.a5046.ui.theme._5046Theme
+
+import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,10 +65,194 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    _5046Theme {
+//        Greeting("Android")
+//    }
+//}
+
+
+
 @Composable
-fun GreetingPreview() {
-    _5046Theme {
-        Greeting("Android")
+fun LoginScreen()  {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    Surface(
+        modifier = Modifier.fillMaxSize(), color = Color(0xFFF1F7F5)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize().padding(32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.plantimg),
+                contentDescription = "Logo",
+                modifier = Modifier.size(180.dp)
+            )
+            Text(
+                text = "Welcome to PlantEase",
+                fontSize = 24.sp,
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 24.dp)
+            )
+            Text(
+                text = "Email",
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.align(Alignment.Start)
+            )
+
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Enter your email") },
+                singleLine = true,
+                modifier = Modifier.align(Alignment.Start).fillMaxWidth()
+            )
+            Text(
+                text = "Password",
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.align(Alignment.Start).padding(top = 10.dp)
+            )
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { email = it },
+                label = { Text("Enter your password") },
+                singleLine = true,
+                modifier = Modifier.align(Alignment.Start).fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Button(
+                onClick = {},
+                modifier = Modifier.fillMaxWidth().padding(vertical = 16.dp).height(48.dp),
+                shape = RoundedCornerShape(12.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF3A915D))
+            ) {
+                Text("Sign in", color = Color.White,fontSize = 18.sp)
+            }
+            Row (modifier = Modifier.fillMaxWidth().padding(top = 24.dp), horizontalArrangement = Arrangement.Center){
+                Text("Don't have an account? ")
+                Text(
+                    text = "SIGN UP",
+                    color = Color(0xFF3A915D),
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+        }
     }
 }
+@Preview(showBackground = true)
+@Composable
+fun LoginPreview() {
+    _5046Theme {
+        LoginScreen()
+    }
+}
+
+@Composable
+fun RegisterScreen()  {
+    var name by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+    Surface(
+        modifier = Modifier.fillMaxSize(), color = Color(0xFFF1F7F5)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize().padding(32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Create your account",
+                fontSize = 24.sp,
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 24.dp)
+            )
+            Text(
+                text = "Name",
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.align(Alignment.Start)
+            )
+            OutlinedTextField(
+                value = name,
+                onValueChange = { name = it },
+                label = { Text("Enter your name") },
+                singleLine = true,
+                modifier = Modifier.align(Alignment.Start).fillMaxWidth()
+            )
+            Text(
+                text = "Email",
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.align(Alignment.Start).padding(top = 10.dp)
+            )
+
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Enter your email") },
+                singleLine = true,
+                modifier = Modifier.align(Alignment.Start).fillMaxWidth()
+            )
+            Text(
+                text = "Password",
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.align(Alignment.Start).padding(top = 10.dp)
+            )
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Enter your password") },
+                singleLine = true,
+                modifier = Modifier.align(Alignment.Start).fillMaxWidth()
+            )
+            Text(
+                text = "Confirm Password",
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.align(Alignment.Start).padding(top = 10.dp)
+            )
+
+            OutlinedTextField(
+                value = confirmPassword,
+                onValueChange = { confirmPassword = it },
+                label = { Text("Re-enter your password") },
+                singleLine = true,
+                modifier = Modifier.align(Alignment.Start).fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Button(
+                onClick = {},
+                modifier = Modifier.fillMaxWidth().padding(vertical = 16.dp).height(48.dp),
+                shape = RoundedCornerShape(12.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF3A915D))
+            ) {
+                Text("Sign up", color = Color.White,fontSize = 18.sp)
+            }
+            Row (modifier = Modifier.fillMaxWidth().padding(top = 24.dp), horizontalArrangement = Arrangement.Center){
+                Text("Have an account ")
+                Text(
+                    text = "SIGN IN",
+                    color = Color(0xFF3A915D),
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RegisterPreview() {
+    _5046Theme {
+        RegisterScreen()
+    }
+}
+
