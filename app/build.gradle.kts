cb31a5d5043bf564d067bcbf608a4749c746c90b
StevenT899 <502845705@qq.com>
2025-05-19 20:58:56 +1000

    merge main

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,5 +95,8 @@ dependencies {
     // Activity Lifecycle
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
 }
 
